f1a32e5acbdde34c2a4e365dc9acc078dd937121
Marco Luglio <marcodejulho@gmail.com>
2021-02-22 18:01:06 -0300

    Adding angular timeline component

--- spa-sample-angular-ts/src/app/languages/languages.module.ts
+++ spa-sample-angular-ts/src/app/languages/languages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LanguagesRoutingModule } from './languages-routing.module';
 import { LanguagesComponent } from './languages.component';
+import { TimelineComponent } from '../timeline/timeline.component';
 
 
 @NgModule({
-	declarations: [LanguagesComponent],
+	declarations: [LanguagesComponent, TimelineComponent],
 	imports: [
 		CommonModule,
 		LanguagesRoutingModule
