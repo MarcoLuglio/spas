d1ede189224f0d47b77ebb3c5a4011b56adf8806
Marco Luglio <marcodejulho@gmail.com>
2021-03-04 22:29:36 -0300

    Adding simple timeline component

---
+++ spa-sample-vue-js/src/views/Languages.vue
@@ -0,0 +1,20 @@
+<template>
+	<p>languages</p>
+	<Timeline :title='timelineTitle' sortBy='name' />
+</template>
+
+<script>
+import Timeline from '@/components/languages/Timeline.vue'
+
+export default {
+	name: 'Languages',
+	components: {
+		Timeline
+	},
+	data() {
+		return {
+			timelineTitle: 'Programming languages'
+		}
+	}
+}
+</script>
